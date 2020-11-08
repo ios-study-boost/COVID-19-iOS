5f83032b6176b8bda3659cec728be0ac2c90a35f
InTaek <dlsxor21c@naver.com>
2020-11-08 20:21:54 +0900

    feat: Init domestic view controller

---
+++ COVID-19-iOS/COVID-19-iOS/DomesticViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DomesticViewController.swift
+//  COVID-19-iOS
+//
+//  Created by In Taek Cho on 2020-11-08.
+//
+
+import UIKit
+
+class DomesticViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
