244f2e65a41d73eed72cf1ecd7a64a3784e22a1b
InTaek <dlsxor21c@naver.com>
2020-11-08 20:28:34 +0900

    feat: Set default setting for table view

--- COVID-19-iOS/COVID-19-iOS/DomesticViewController.swift
+++ COVID-19-iOS/COVID-19-iOS/DomesticViewController.swift
@@ -9,21 +9,23 @@ import UIKit
 
 class DomesticViewController: UIViewController {
 
+    @IBOutlet weak var domesticTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension DomesticViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
-    */
+}
+
+extension DomesticViewController: UITableViewDataSource {
 
 }
