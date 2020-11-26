86d50bf949c92b0e140d254f6acce30fc80af99b
devejs <ssej0221@gmail.com>
2020-11-26 10:06:38 +0900

    feat: Add segmented control and activate it

--- COVID-19-iOS/COVID-19-iOS/Area.swift
+++ COVID-19-iOS/COVID-19-iOS/Area.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+//extension Area {
+//    func add(_ added :Area){
+//        self.death
+//    }
+//}
+
+struct Area {
+
+    let deathCnt : Int
+    let areaName : String
+    let increaseCnt : Int
+    let isolClear : Int
+    let numInfected : Int
+    let createDate : String
+    
+    init(deathCnt: String, areaName: String, incDec: String, isolClearCnt: String, qurRate: String, createDt: String){
+        self.deathCnt = Int(deathCnt) ?? 0
+        self.areaName = areaName
+        self.increaseCnt = Int(incDec) ?? 0
+        self.isolClear = Int(isolClearCnt) ?? 0
+        self.numInfected = Int((Double(qurRate) ?? 0.0 * 10000000))
+        self.createDate = createDt
+    
+    }
+    
+    func getAreaDetail()-> String{
+        return "확진자: \(self.numInfected), \(self.increaseCnt)"
+    }
+}
--- COVID-19-iOS/COVID-19-iOS/TableViewController.swift
+++ COVID-19-iOS/COVID-19-iOS/TableViewController.swift
@@ -6,24 +6,123 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyXMLParser
 
-class TableViewController: UIViewController {
 
+class TableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    var areas : [String: Area] = [:]
+    var areaNames : [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        testForAPI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.areas.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RegionCell", for: indexPath)
+        
+        return cell
+    }
+    
+    func testForAPI() {
+        let baseURL = "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson"
 
+        let params: [String:Any] = ["serviceKey":"yOnwo%2Bo9O9KpHIrKU1jZIyPCFdlRVkLIsa714mrNIOfNPQAEVelNNF%2F%2FveF6iRhqmySy4KGM07vdpaU6W8YosA%3D%3D", "pageNo":1, "numOfRows":10, "startCreateDt":20200310, "endCreateDt":20200315]
+        
+        func getURL(url:String, params:[String: Any]) -> URL {
+            let urlParams = params.compactMap({ (key, value) -> String in
+                return "\(key)=\(value)"
+            }).joined(separator: "&")
+            let withURL = url + "?\(urlParams)"
+            
+            return URL(string: withURL)!
+        }
+        
+        func getCoronaData(params: [String:Any]) {
+            let url = getURL(url: baseURL, params: params)
+            
+            let request = AF.request(url, method: .get).validate()
+            
+            request.responseString { response in
+                //if case success
+                switch response.result {
+                case .success(let value):
+                    let responseString = NSString(data: response.data!, encoding: String.Encoding.utf8.rawValue )
+                    guard let xml = try? XML.parse(String(responseString!)) else {
+                        return
+                    }
+                    for item in xml.response.body.items.item {
+                        let thisArea : String = item.gubun.text ?? ""
+                        self.areaNames.append(thisArea)
+            
+                        let area : Area = Area(deathCnt: item.deathCnt.text ?? "", areaName: item.gubun.text ?? "", incDec: item.incDec.text ?? "", isolClearCnt: item.isolClearCnt.text ?? "", qurRate: item.qurRate.text ?? "", createDt: item.createDt.text ?? "")
+                        print("\(item.qurRate.text)")
+                        if self.areas[thisArea] != nil{
+                            
+                        }else{
+                            self.areas[thisArea] = area
+                        }
+                    }
+                case .failure(let error):
+                    print(error)
+                }
+            }
+        }
+        getCoronaData(params: params)
+        //testForXMLParser()
+    }
+
+    func testForXMLParser() {
+            let str = """
+            <ResultSet>
+                <Result>
+                    <Hit index=\"1\">
+                        <Name>Item1</Name>
+                    </Hit>
+                    <Hit index=\"2\">
+                        <Name>Item2</Name>
+                    </Hit>
+                </Result>
+            </ResultSet>
+            """
+            
+            // parse xml document
+            let xml = try! XML.parse(str)
+            
+            // access xml element
+            let accessor = xml["ResultSet"]
+            
+            // access XML Text
+            
+            if let text = xml["ResultSet", "Result", "Hit", 0, "Name"].text {
+                print(text)
+            }
+            
+            if let text = xml.ResultSet.Result.Hit[0].Name.text {
+                print(text)
+            }
+            
+            // access XML Attribute
+            if let index = xml["ResultSet", "Result", "Hit", 0].attributes["index"] {
+                print(index)
+            }
+            
+            // enumerate child Elements in the parent Element
+            for hit in xml["ResultSet", "Result", "Hit"] {
+                print(hit)
+            }
+            
+            // check if the XML path is wrong
+            if case .failure(let error) =  xml["ResultSet", "Result", "TypoKey"] {
+                print(error)
+            }
+        }
+    
 }
